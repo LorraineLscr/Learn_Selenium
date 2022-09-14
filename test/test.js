c5adcc4027e14337d4b50098f4fe791619d7f95b
LorraineLscr <lorraiine76@hotmail.fr>
2022-09-14 12:06:30 +0200

    ✨ Code test

--- test/test.js
+++ test/test.js
@@ -1,29 +1,39 @@
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 
-// On crée une fonction asynchrone
-async function example() {
+// Description du block
+describe('add todo tests', () => {
 
-    // Ici on donne un exemple de recherche dans le navigateur
-    var searchString = "Automation testing with Selenium and JavaScript";
+    // Le block
+    it("successfully adds a todo to application", async function () {
+        
+        // Ici on donne un exemple de recherche dans le navigateur
+        var searchString = "Automation testing with Selenium and JavaScript";
 
-    // On crée une nouvelle instance de navigateur pour Google Chrome
-    let driver = await new Builder().forBrowser("chrome").build();
+        // On crée une nouvelle instance de navigateur pour Google Chrome
+        let driver = await new Builder().forBrowser("chrome").build();
 
-    // On navigue vers l'application Web à l'aide du driver.get
-    await driver.get("http://google.com");
+        // On navigue vers l'application Web à l'aide du driver.get
+        await driver.get("http://google.com");
 
-    // On envoie une requête de recherche en passant la valeur dans searchString
-    await driver.findElement(By.id("L2AGLb")).sendKeys(Key.RETURN);
-    await driver.findElement(By.name("q")).sendKeys(searchString, Key.RETURN);
+        // On envoie une requête de recherche en passant la valeur dans searchString
+        await driver.findElement(By.id("L2AGLb")).sendKeys(Key.RETURN);
+        await driver.findElement(By.name("q")).sendKeys(searchString, Key.RETURN);
 
-    // On vérifie le titre de la page et on l'affiche dans le terminal
-    var title = await driver.getTitle();
-    console.log('Le titre est :', title);
+        // On vérifie le titre de la page et on l'affiche dans le terminal
+        var title = await driver.getTitle();
+        console.log('Le titre est :', title);
 
-    // On utilise driver.quit pour fermer le navigateur
-    await driver.quit();
+        // On utilise driver.quit pour fermer le navigateur
+        await driver.quit();
+        
+    });
+});
 
-}
 
-example()
+
+
+
+
+
+
